c538cb48cae7f630750427ee62d814bc2010b35f
Dauda Lawal <64631869+Daslaw@users.noreply.github.com>
2023-01-17 12:58:16 +0100

    Adding of Delete configuration

--- src/App.js
+++ src/App.js
@@ -17,18 +17,40 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
   
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
 
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
 
+  
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
   
 
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -38,7 +60,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ export default function Todo(props) {
     return (
       <li className="todo stack-small">
        <div className="c-cb">
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+            <input id={props.id} type="checkbox" defaultChecked={props.completed} onChange={() => props.toggleTaskCompleted(props.id)} />
             <label className="todo-label" htmlFor={props.id}>
                 {props.name}
             </label>
@@ -14,9 +14,12 @@ export default function Todo(props) {
           <button type="button" className="btn">
             Edit <span className="visually-hidden">{props.name}</span>
           </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">{props.name}</span>
-          </button>
+          <button
+                  type="button"
+                  className="btn btn__danger"
+                  onClick={() => props.deleteTask(props.id)}>
+                  Delete <span className="visually-hidden">{props.name}</span>
+            </button>
         </div>
       </li>
     );
